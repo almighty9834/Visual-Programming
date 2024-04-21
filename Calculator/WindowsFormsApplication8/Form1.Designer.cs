66dcd7753f650d7c00f222243ac4fb6144b987d4
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-04-22 02:39:05 +0500

    Add files via upload

---
+++ Calculator/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBox = new System.Windows.Forms.TextBox();
+            this.SevenBtn = new System.Windows.Forms.Button();
+            this.FourBtn = new System.Windows.Forms.Button();
+            this.OneBtn = new System.Windows.Forms.Button();
+            this.ZeroBtn = new System.Windows.Forms.Button();
+            this.DotBtn = new System.Windows.Forms.Button();
+            this.TwoBtn = new System.Windows.Forms.Button();
+            this.FiveBtn = new System.Windows.Forms.Button();
+            this.EightBtn = new System.Windows.Forms.Button();
+            this.EqualBtn = new System.Windows.Forms.Button();
+            this.ThreeBtn = new System.Windows.Forms.Button();
+            this.SixBtn = new System.Windows.Forms.Button();
+            this.NineBtn = new System.Windows.Forms.Button();
+            this.ModulesBtn = new System.Windows.Forms.Button();
+            this.MuliplyBtn = new System.Windows.Forms.Button();
+            this.PlusBtn = new System.Windows.Forms.Button();
+            this.MinusPlusBtn = new System.Windows.Forms.Button();
+            this.DevideBtn = new System.Windows.Forms.Button();
+            this.MinusBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBox
+            // 
+            this.TxtBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBox.Location = new System.Drawing.Point(-3, 25);
+            this.TxtBox.Multiline = true;
+            this.TxtBox.Name = "TxtBox";
+            this.TxtBox.Size = new System.Drawing.Size(397, 53);
+            this.TxtBox.TabIndex = 0;
+            this.TxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.TxtBox.TextChanged += new System.EventHandler(this.TxtBox_TextChanged);
+            // 
+            // SevenBtn
+            // 
+            this.SevenBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SevenBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SevenBtn.Location = new System.Drawing.Point(12, 140);
+            this.SevenBtn.Name = "SevenBtn";
+            this.SevenBtn.Size = new System.Drawing.Size(65, 52);
+            this.SevenBtn.TabIndex = 1;
+            this.SevenBtn.Text = "7";
+            this.SevenBtn.UseVisualStyleBackColor = false;
+            this.SevenBtn.Click += new System.EventHandler(this.SevenBtn_Click);
+            // 
+            // FourBtn
+            // 
+            this.FourBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FourBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FourBtn.Location = new System.Drawing.Point(12, 198);
+            this.FourBtn.Name = "FourBtn";
+            this.FourBtn.Size = new System.Drawing.Size(65, 55);
+            this.FourBtn.TabIndex = 2;
+            this.FourBtn.Text = "4";
+            this.FourBtn.UseVisualStyleBackColor = false;
+            this.FourBtn.Click += new System.EventHandler(this.FourBtn_Click);
+            // 
+            // OneBtn
+            // 
+            this.OneBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.OneBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OneBtn.Location = new System.Drawing.Point(12, 259);
+            this.OneBtn.Name = "OneBtn";
+            this.OneBtn.Size = new System.Drawing.Size(65, 51);
+            this.OneBtn.TabIndex = 3;
+            this.OneBtn.Text = "1";
+            this.OneBtn.UseVisualStyleBackColor = false;
+            this.OneBtn.Click += new System.EventHandler(this.OneBtn_Click);
+            // 
+            // ZeroBtn
+            // 
+            this.ZeroBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ZeroBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZeroBtn.Location = new System.Drawing.Point(12, 316);
+            this.ZeroBtn.Name = "ZeroBtn";
+            this.ZeroBtn.Size = new System.Drawing.Size(65, 56);
+            this.ZeroBtn.TabIndex = 4;
+            this.ZeroBtn.Text = "0";
+            this.ZeroBtn.UseVisualStyleBackColor = false;
+            this.ZeroBtn.Click += new System.EventHandler(this.ZeroBtn_Click);
+            // 
+            // DotBtn
+            // 
+            this.DotBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.DotBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DotBtn.Location = new System.Drawing.Point(83, 316);
+            this.DotBtn.Name = "DotBtn";
+            this.DotBtn.Size = new System.Drawing.Size(65, 56);
+            this.DotBtn.TabIndex = 8;
+            this.DotBtn.Text = ".";
+            this.DotBtn.UseVisualStyleBackColor = false;
+            this.DotBtn.Click += new System.EventHandler(this.DotBtn_Click);
+            // 
+            // TwoBtn
+            // 
+            this.TwoBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.TwoBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TwoBtn.Location = new System.Drawing.Point(83, 259);
+            this.TwoBtn.Name = "TwoBtn";
+            this.TwoBtn.Size = new System.Drawing.Size(65, 51);
+            this.TwoBtn.TabIndex = 7;
+            this.TwoBtn.Text = "2";
+            this.TwoBtn.UseVisualStyleBackColor = false;
+            this.TwoBtn.Click += new System.EventHandler(this.TwoBtn_Click);
+            // 
+            // FiveBtn
+            // 
+            this.FiveBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FiveBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiveBtn.Location = new System.Drawing.Point(83, 198);
+            this.FiveBtn.Name = "FiveBtn";
+            this.FiveBtn.Size = new System.Drawing.Size(65, 55);
+            this.FiveBtn.TabIndex = 6;
+            this.FiveBtn.Text = "5";
+            this.FiveBtn.UseVisualStyleBackColor = false;
+            this.FiveBtn.Click += new System.EventHandler(this.FiveBtn_Click);
+            // 
+            // EightBtn
+            // 
+            this.EightBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.EightBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EightBtn.Location = new System.Drawing.Point(83, 140);
+            this.EightBtn.Name = "EightBtn";
+            this.EightBtn.Size = new System.Drawing.Size(65, 52);
+            this.EightBtn.TabIndex = 5;
+            this.EightBtn.Text = "8";
+            this.EightBtn.UseVisualStyleBackColor = false;
+            this.EightBtn.Click += new System.EventHandler(this.EightBtn_Click);
+            // 
+            // EqualBtn
+            // 
+            this.EqualBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.EqualBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EqualBtn.Location = new System.Drawing.Point(154, 316);
+            this.EqualBtn.Name = "EqualBtn";
+            this.EqualBtn.Size = new System.Drawing.Size(65, 56);
+            this.EqualBtn.TabIndex = 12;
+            this.EqualBtn.Text = "=";
+            this.EqualBtn.UseVisualStyleBackColor = false;
+            this.EqualBtn.Click += new System.EventHandler(this.EqualBtn_Click);
+            // 
+            // ThreeBtn
+            // 
+            this.ThreeBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ThreeBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThreeBtn.Location = new System.Drawing.Point(154, 259);
+            this.ThreeBtn.Name = "ThreeBtn";
+            this.ThreeBtn.Size = new System.Drawing.Size(65, 51);
+            this.ThreeBtn.TabIndex = 11;
+            this.ThreeBtn.Text = "3";
+            this.ThreeBtn.UseVisualStyleBackColor = false;
+            this.ThreeBtn.Click += new System.EventHandler(this.ThreeBtn_Click);
+            // 
+            // SixBtn
+            // 
+            this.SixBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SixBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SixBtn.Location = new System.Drawing.Point(154, 198);
+            this.SixBtn.Name = "SixBtn";
+            this.SixBtn.Size = new System.Drawing.Size(65, 55);
+            this.SixBtn.TabIndex = 10;
+            this.SixBtn.Text = "6";
+            this.SixBtn.UseVisualStyleBackColor = false;
+            this.SixBtn.Click += new System.EventHandler(this.SixBtn_Click);
+            // 
+            // NineBtn
+            // 
+            this.NineBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.NineBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NineBtn.Location = new System.Drawing.Point(154, 140);
+            this.NineBtn.Name = "NineBtn";
+            this.NineBtn.Size = new System.Drawing.Size(65, 52);
+            this.NineBtn.TabIndex = 9;
+            this.NineBtn.Text = "9";
+            this.NineBtn.UseVisualStyleBackColor = false;
+            this.NineBtn.Click += new System.EventHandler(this.NineBtn_Click);
+            // 
+            // ModulesBtn
+            // 
+            this.ModulesBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ModulesBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModulesBtn.Location = new System.Drawing.Point(314, 316);
+            this.ModulesBtn.Name = "ModulesBtn";
+            this.ModulesBtn.Size = new System.Drawing.Size(65, 56);
+            this.ModulesBtn.TabIndex = 16;
+            this.ModulesBtn.Text = "%";
+            this.ModulesBtn.UseVisualStyleBackColor = false;
+            this.ModulesBtn.Click += new System.EventHandler(this.ModulesBtn_Click);
+            // 
+            // MuliplyBtn
+            // 
+            this.MuliplyBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MuliplyBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MuliplyBtn.Location = new System.Drawing.Point(314, 259);
+            this.MuliplyBtn.Name = "MuliplyBtn";
+            this.MuliplyBtn.Size = new System.Drawing.Size(65, 51);
+            this.MuliplyBtn.TabIndex = 15;
+            this.MuliplyBtn.Text = "*";
+            this.MuliplyBtn.UseVisualStyleBackColor = false;
+            this.MuliplyBtn.Click += new System.EventHandler(this.MuliplyBtn_Click);
+            // 
+            // PlusBtn
+            // 
+            this.PlusBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.PlusBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlusBtn.Location = new System.Drawing.Point(314, 198);
+            this.PlusBtn.Name = "PlusBtn";
+            this.PlusBtn.Size = new System.Drawing.Size(65, 55);
+            this.PlusBtn.TabIndex = 14;
+            this.PlusBtn.Text = "+";
+            this.PlusBtn.UseVisualStyleBackColor = false;
+            this.PlusBtn.Click += new System.EventHandler(this.PlusBtn_Click);
+            // 
+            // MinusPlusBtn
+            // 
+            this.MinusPlusBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MinusPlusBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinusPlusBtn.Location = new System.Drawing.Point(243, 316);
+            this.MinusPlusBtn.Name = "MinusPlusBtn";
+            this.MinusPlusBtn.Size = new System.Drawing.Size(65, 56);
+            this.MinusPlusBtn.TabIndex = 20;
+            this.MinusPlusBtn.Text = "-/+";
+            this.MinusPlusBtn.UseVisualStyleBackColor = false;
+            this.MinusPlusBtn.Click += new System.EventHandler(this.MinusPlusBtn_Click);
+            // 
+            // DevideBtn
+            // 
+            this.DevideBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.DevideBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DevideBtn.Location = new System.Drawing.Point(243, 259);
+            this.DevideBtn.Name = "DevideBtn";
+            this.DevideBtn.Size = new System.Drawing.Size(65, 51);
+            this.DevideBtn.TabIndex = 19;
+            this.DevideBtn.Text = "/";
+            this.DevideBtn.UseVisualStyleBackColor = false;
+            this.DevideBtn.Click += new System.EventHandler(this.DevideBtn_Click);
+            // 
+            // MinusBtn
+            // 
+            this.MinusBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MinusBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinusBtn.Location = new System.Drawing.Point(243, 198);
+            this.MinusBtn.Name = "MinusBtn";
+            this.MinusBtn.Size = new System.Drawing.Size(65, 55);
+            this.MinusBtn.TabIndex = 18;
+            this.MinusBtn.Text = "-";
+            this.MinusBtn.UseVisualStyleBackColor = false;
+            this.MinusBtn.Click += new System.EventHandler(this.MinusBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClearBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearBtn.Location = new System.Drawing.Point(243, 140);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(136, 52);
+            this.ClearBtn.TabIndex = 17;
+            this.ClearBtn.Text = "AC";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(393, 427);
+            this.Controls.Add(this.MinusPlusBtn);
+            this.Controls.Add(this.DevideBtn);
+            this.Controls.Add(this.MinusBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.ModulesBtn);
+            this.Controls.Add(this.MuliplyBtn);
+            this.Controls.Add(this.PlusBtn);
+            this.Controls.Add(this.EqualBtn);
+            this.Controls.Add(this.ThreeBtn);
+            this.Controls.Add(this.SixBtn);
+            this.Controls.Add(this.NineBtn);
+            this.Controls.Add(this.DotBtn);
+            this.Controls.Add(this.TwoBtn);
+            this.Controls.Add(this.FiveBtn);
+            this.Controls.Add(this.EightBtn);
+            this.Controls.Add(this.ZeroBtn);
+            this.Controls.Add(this.OneBtn);
+            this.Controls.Add(this.FourBtn);
+            this.Controls.Add(this.SevenBtn);
+            this.Controls.Add(this.TxtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBox;
+        private System.Windows.Forms.Button SevenBtn;
+        private System.Windows.Forms.Button FourBtn;
+        private System.Windows.Forms.Button OneBtn;
+        private System.Windows.Forms.Button ZeroBtn;
+        private System.Windows.Forms.Button DotBtn;
+        private System.Windows.Forms.Button TwoBtn;
+        private System.Windows.Forms.Button FiveBtn;
+        private System.Windows.Forms.Button EightBtn;
+        private System.Windows.Forms.Button EqualBtn;
+        private System.Windows.Forms.Button ThreeBtn;
+        private System.Windows.Forms.Button SixBtn;
+        private System.Windows.Forms.Button NineBtn;
+        private System.Windows.Forms.Button ModulesBtn;
+        private System.Windows.Forms.Button MuliplyBtn;
+        private System.Windows.Forms.Button PlusBtn;
+        private System.Windows.Forms.Button MinusPlusBtn;
+        private System.Windows.Forms.Button DevideBtn;
+        private System.Windows.Forms.Button MinusBtn;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
