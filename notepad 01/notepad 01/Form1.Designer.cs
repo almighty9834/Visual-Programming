8a1bb5346d4205a92346dbc6c89afee0bf00ff1f
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-05-03 01:52:31 +0500

    Add files via upload

---
+++ notepad 01/notepad 01/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace notepad_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(2, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(956, 658);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // chkBold
+            // 
+            this.chkBold.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkBold.AutoSize = true;
+            this.chkBold.Location = new System.Drawing.Point(146, 12);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(38, 23);
+            this.chkBold.TabIndex = 1;
+            this.chkBold.Text = "Bold";
+            this.chkBold.UseVisualStyleBackColor = true;
+            this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Font";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(79, 12);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(61, 23);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(190, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(61, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(324, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(61, 23);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(257, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(61, 23);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Open ";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 711);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkBold);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ notepad 01/notepad 01/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+
+            Font font = richTextBox1.SelectionFont;
+            font = new Font(font.FontFamily ,font.Size,FontStyle.Bold);
+                richTextBox1.SelectionFont= font;
+
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnColor_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog clrdilg = new ColorDialog();
+            if (clrdilg.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = clrdilg.Color;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svfildilg = new SaveFileDialog();
+            svfildilg.Filter = "*.rtf|Rich Text Format";
+            svfildilg.DefaultExt = "rtf";
+            if (svfildilg.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(svfildilg.FileName, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Rich Text Format|*.rtf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string txt = File.ReadAllText(ofd.FileName);
+                richTextBox1.Rtf = File.ReadAllText(ofd.FileName);
+            }
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            {
+             richTextBox1.Clear();
+            }
+        }
+    }
+}
