b8b9748e4ab24fec354145bd889dfc342051e088
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-05-02 23:02:16 +0500

    Add files via upload

---
+++ Masked TextBox/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = maskedTextBox1.Text;
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            label1.Text = "Invalid Charater ";
+        }
+    }
+}
