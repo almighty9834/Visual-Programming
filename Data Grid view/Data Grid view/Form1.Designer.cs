772ed5fcaf350aa8149e97ef48b7ecfaf8e7c221
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-05-10 17:42:14 +0500

    Add files via upload

---
+++ Data Grid view/Data Grid view/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Data_Grid_view
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bhtload = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(819, 302);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bhtload
+            // 
+            this.bhtload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bhtload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bhtload.Location = new System.Drawing.Point(33, 378);
+            this.bhtload.Name = "bhtload";
+            this.bhtload.Size = new System.Drawing.Size(98, 53);
+            this.bhtload.TabIndex = 1;
+            this.bhtload.Text = "Load";
+            this.bhtload.UseVisualStyleBackColor = true;
+            this.bhtload.Click += new System.EventHandler(this.bhtload_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(192, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "load CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 454);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bhtload);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bhtload;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Data Grid view/Data Grid view/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_Grid_view
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bhtload_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("RollNo", typeof(Int32));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Father Name", typeof(string));
+            dt.Columns.Add("Address", typeof(string));
+            dt.Columns.Add("E-Mial", typeof(string));
+            dt.Columns.Add("Phone No.", typeof(string));
+
+            dt.Columns.Add("Class", typeof(string));
+            dt.Columns.Add("Session", typeof(string));
+
+            dt.Columns.Add("Inter Marks", typeof(string));
+            dt.Columns.Add("Obtain Marks", typeof(string));
+            dt.Columns.Add("Grade", typeof(string));
+
+            dt.Rows.Add(2, "Ayesha", "Mukhtar", "BScs 5th Semester", "Arif town, street#07", "Ayesha@gmail.com", "03038-343434", "2023-2025", "1100", "850", "A");
+            dt.Rows.Add(1, "Sehar", "Mukhtar", "BS 1st Semester", "Arif town, street#07", "Sehar@gmail.com", "03038-343434", "2023-2025", "1100", "980", "A+");
+            dt.Rows.Add(4, "Ushna", "Hafeez", " BScs 5th Semester", "", "Ushna@gmail.com", "03038-343434", "2023-2025", "1100", "896", "A");
+
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\netflix_titles";
+            string[] lines = File.ReadAllLines(path);
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("col1", "id");
+            dataGridView1.Columns.Add("col2", "type");
+            dataGridView1.Columns.Add("col3", "title");
+            dataGridView1.Columns.Add("col4", "director");
+            dataGridView1.Columns.Add("col5", "cast");
+            dataGridView1.Columns.Add("col6", "country");
+            dataGridView1.Columns.Add("col7", "data_added");
+            dataGridView1.Columns.Add("col8", "release year");
+            dataGridView1.Columns.Add("col9", "rating ");
+            dataGridView1.Columns.Add("col10", "duration");
+            dataGridView1.Columns.Add("col11", "Listed _in");
+            dataGridView1.Columns.Add("col12", "description");
+
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                string id = columns[0];
+                string type = columns[1];
+                string title = columns[2];
+                string director = columns[3];
+                string cast= columns[4];
+                string country = columns[5];
+                string data_added = columns[6];
+                string releaseyear = columns[7];
+                string rating = columns[8];
+                string duration  = columns[9];
+                string listed_in = columns[10];
+                string description  = columns[11];
+
+
+
+
+
+
+                dataGridView1.Rows.Add(id, type, title, director, cast, country, data_added, releaseyear, rating, duration, listed_in, description);
+            }
+        }
+    }
+}
