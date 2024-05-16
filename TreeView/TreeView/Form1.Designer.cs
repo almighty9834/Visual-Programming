3848d30092fb7367c0ddc433f571de501ceca60e
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-05-17 00:10:52 +0500

    Add files via upload

---
+++ TreeView/TreeView/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageListlarge = new System.Windows.Forms.ImageList(this.components);
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.rdoLargeIcon = new System.Windows.Forms.RadioButton();
+            this.rdoSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rdoDetails = new System.Windows.Forms.RadioButton();
+            this.rdoTile = new System.Windows.Forms.RadioButton();
+            this.rdoList = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 39);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(335, 609);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.LargeImageList = this.imageListlarge;
+            this.listView1.Location = new System.Drawing.Point(331, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(510, 609);
+            this.listView1.SmallImageList = this.imageListSmall;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // imageListlarge
+            // 
+            this.imageListlarge.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListlarge.ImageStream")));
+            this.imageListlarge.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListlarge.Images.SetKeyName(0, "icons8-message-16.png");
+            // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmall.ImageStream")));
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListSmall.Images.SetKeyName(0, "icons8-message-48.png");
+            // 
+            // rdoLargeIcon
+            // 
+            this.rdoLargeIcon.AutoSize = true;
+            this.rdoLargeIcon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoLargeIcon.Location = new System.Drawing.Point(38, 3);
+            this.rdoLargeIcon.Name = "rdoLargeIcon";
+            this.rdoLargeIcon.Size = new System.Drawing.Size(99, 36);
+            this.rdoLargeIcon.TabIndex = 2;
+            this.rdoLargeIcon.Text = "Large Icon\r\n\r\n";
+            this.rdoLargeIcon.UseVisualStyleBackColor = true;
+            this.rdoLargeIcon.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoSmallIcon
+            // 
+            this.rdoSmallIcon.AutoSize = true;
+            this.rdoSmallIcon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoSmallIcon.Location = new System.Drawing.Point(143, 3);
+            this.rdoSmallIcon.Name = "rdoSmallIcon";
+            this.rdoSmallIcon.Size = new System.Drawing.Size(98, 36);
+            this.rdoSmallIcon.TabIndex = 3;
+            this.rdoSmallIcon.Text = "Small Icon\r\n\r\n";
+            this.rdoSmallIcon.UseVisualStyleBackColor = true;
+            this.rdoSmallIcon.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoDetails
+            // 
+            this.rdoDetails.AutoSize = true;
+            this.rdoDetails.Checked = true;
+            this.rdoDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoDetails.Location = new System.Drawing.Point(247, 9);
+            this.rdoDetails.Name = "rdoDetails";
+            this.rdoDetails.Size = new System.Drawing.Size(78, 22);
+            this.rdoDetails.TabIndex = 4;
+            this.rdoDetails.TabStop = true;
+            this.rdoDetails.Text = "Details";
+            this.rdoDetails.UseVisualStyleBackColor = true;
+            this.rdoDetails.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoTile
+            // 
+            this.rdoTile.AutoSize = true;
+            this.rdoTile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoTile.Location = new System.Drawing.Point(331, 9);
+            this.rdoTile.Name = "rdoTile";
+            this.rdoTile.Size = new System.Drawing.Size(62, 22);
+            this.rdoTile.TabIndex = 5;
+            this.rdoTile.Text = "Tiles";
+            this.rdoTile.UseVisualStyleBackColor = true;
+            this.rdoTile.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoList
+            // 
+            this.rdoList.AutoSize = true;
+            this.rdoList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoList.Location = new System.Drawing.Point(399, 9);
+            this.rdoList.Name = "rdoList";
+            this.rdoList.Size = new System.Drawing.Size(53, 22);
+            this.rdoList.TabIndex = 6;
+            this.rdoList.Text = "List";
+            this.rdoList.UseVisualStyleBackColor = true;
+            this.rdoList.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 696);
+            this.Controls.Add(this.rdoList);
+            this.Controls.Add(this.rdoTile);
+            this.Controls.Add(this.rdoDetails);
+            this.Controls.Add(this.rdoSmallIcon);
+            this.Controls.Add(this.rdoLargeIcon);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageListlarge;
+        private System.Windows.Forms.ImageList imageListSmall;
+        private System.Windows.Forms.RadioButton rdoLargeIcon;
+        private System.Windows.Forms.RadioButton rdoSmallIcon;
+        private System.Windows.Forms.RadioButton rdoDetails;
+        private System.Windows.Forms.RadioButton rdoTile;
+        private System.Windows.Forms.RadioButton rdoList;
+    }
+}
+
