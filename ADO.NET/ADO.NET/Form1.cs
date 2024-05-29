440c8e986411827001b756a25613a9761c985756
Ayesha Mukhtar <167670726+almighty9834@users.noreply.github.com>
2024-05-29 23:56:13 +0500

    Add files via upload

---
+++ ADO.NET/ADO.NET/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ADO.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.Cursor = Cursors.WaitCursor;
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SONIC-PC\\MSSSQLSERVER; Initial Catalog=College; Integrated Security=True;";
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Connected Successfully");
+
+
+                  SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from Student";
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(conn.State!=ConnectionState.Closed)
+                conn.Close();
+
+                this.Cursor = Cursors.Default;
+            }
+        }
+    }
+}
